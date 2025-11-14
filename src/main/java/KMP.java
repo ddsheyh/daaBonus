b9d2e7e1a92daa3d93b262116f46fee9725c8487
ddsheyh <243077@astanait.edu.kz>
2025-11-14 12:55:14 +0500

    Implement computeLPSArray for KMP preprocessing

--- src/main/java/KMP.java
+++ src/main/java/KMP.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class KMP {
+    private int[] computeLPSArray(String pattern) {
+        int M = pattern.length();
+        int[] lps = new int[M];
+
+        int length = 0;
+        int i = 1;
+        lps[0] = 0;
+
+        while (i < M) {
+            if (pattern.charAt(i) == pattern.charAt(length)) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+
 }
